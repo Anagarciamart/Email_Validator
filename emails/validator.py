92d3bba94ebee1de496f3363150fb09310d83d86
Ana <9102885@alumnos.ufv.es>
2024-11-17 19:32:11 +0100

    Modificado

--- emails/validator.py
+++ emails/validator.py
@@ -1,8 +1,27 @@
-
-## TODO Gestión de excepciones
 import re
+import logging
+
+logging.basicConfig(
+    filename='logs/mail_validator.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 class EmailValidator:
     def __init__(self):
         self.regex  = re.compile(r'([A-Za-z]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
     def validar(self,correo:str):
-        return re.fullmatch(self.regex, correo)
+        try:
+            if not isinstance(correo, str):
+                raise ValueError("El correo debe ser una cadena de texto")
+            if re.fullmatch(self.regex, correo):
+                logging.info(f"{correo} es válido")
+                return True
+            else:
+                logging.warning(f"{correo} no es válido")
+                return False
+        except ValueError as ve:
+            logging.error(f"Error de valor: {ve}")
+            raise
+        except Exception as e:
+            logging.error(f"Error inesperado: {e}")
+            raise
